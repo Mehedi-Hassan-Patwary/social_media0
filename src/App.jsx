d98dccb286c1c59fbc22fb137e728492d2ba0ec7
Mehedi-Hassan-Patwary <md.mehedi.hassan.patwary.cse@gmail.com>
2024-07-16 22:45:48 -0700

    Design pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-
+import React, { useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header'; 
+import Footer from './components/Footer';
+import Saidebar from './components/Saidebar';
+import CreatePost from './components/CreatePost';
+import PostList from './components/PostList';
+
+
 
 function App() {
 
+   const [selectedTab, setSelectedTab] = useState("Home");
+
   return (
-    <>
-        <div>Place Holder</div>
-    </>
-  )
-}
+    <div className="app-container">  
+        <Saidebar></Saidebar>
+        <div className="content">
+              <Header></Header>
+              {selectedTab === 'Home' ? <PostList></PostList> : <CreatePost/> }
+              
+              
+              <Footer></Footer>
+
+        </div>   
+    </div>
+  );
+};
 
 export default App;
